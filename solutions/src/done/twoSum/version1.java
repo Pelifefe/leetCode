3a592b81de54d71c19e68fb5b2d44f59b077aed0
Felippe Pedrosa <136206263+pelifefe@users.noreply.github.com>
2026-01-21 20:59:21 -0300

    Initial commit

---
+++ solutions/src/done/twoSum/version1.java
@@ -0,0 +1,17 @@
+package done.twoSum;
+
+public class version1 {
+    public int[] twoSum(int[]nums, int target){
+        int[] output;
+
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    output = new int[]{i, j};
+                    return output;
+                }
+            }
+        }
+        return null;
+    }
+}
